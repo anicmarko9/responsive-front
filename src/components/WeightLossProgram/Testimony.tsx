2dc16c38f09bbd3bc20dbdb9fe1215b75874d81d
BelloDev <fernandojbf123@gmail.com>
2023-08-07 22:08:53 -0700

    fixed issues in weight loss program page as suggested by animarko

--- src/components/WeightLossProgram/Testimony.tsx
+++ src/components/WeightLossProgram/Testimony.tsx
@@ -7,17 +7,19 @@ type Props = {
 };
 
 const Testimony = ({ imgSrc, name, testimony }: Props): JSX.Element => {
+  const notFound = './ImgNotFound.svg';
+
   return (
-    <div className="flex flex-col items-center overflow-y-auto h-[450px]">
+    <div className="flex flex-col items-center overflow-y-auto overflow-x-hidden h-[450px]">
       <figure className="w-[300px] flex flex-col items-center">
         <img
           className="h-[250px] w-[250px] rounded-full"
-          src={imgSrc}
-          alt={`${name}`}
+          src={imgSrc || notFound}
+          alt={name}
         />
         <figcaption className="p-3 text-xl italic flex flex-col">
           {name}
-          {testimony && <p>{testimony}</p>}
+          {testimony && <span className="text-justify">{testimony}</span>}
         </figcaption>
       </figure>
     </div>
