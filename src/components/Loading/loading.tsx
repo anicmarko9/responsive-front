612ec8e032ac286e7bc2339307bb2f429e071c77
Vidhanvyrs <vidhanvyrs@gmail.com>
2023-08-11 21:21:55 +0530

    Added the loading component

---
+++ src/components/Loading/loading.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+const Loading: React.FC = () => {
+  return (
+    <div className="loading-container">
+      <div className="loading-spinner"></div>
+    </div>
+  );
+};
+
+export default Loading;
