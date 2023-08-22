008171d6dc63e1ab0d0c42ebb0effff446c0bf0f
Abdelmonaem <abdelmoname@me.com>
2023-08-22 18:53:38 +0300

    format changes

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -10,11 +10,14 @@ const Footer = (): JSX.Element => {
     <footer className="relative flex h-full flex-col bg-customWhite px-8 pb-8 pt-8 text-base sm:px-16 sm:text-lg xl:px-40 xl:pt-16">
       <div className="mb-4 flex flex-wrap justify-between xl:mb-24 xl:mt-4">
         <section className="w-full space-y-8 pb-8 sm:w-1/2 xl:w-5/12">
-          <a
-            className="block hover:cursor-pointer"
-            href="/"
-          >
-            <img className='duration-300 ease-in-out hover:opacity-70 hover:-translate-y-1 transition-transform active:scale-95' src="/logo.png" alt="Logo" width={140} height={65} />
+          <a className="block hover:cursor-pointer" href="/">
+            <img
+              className="duration-300 ease-in-out hover:opacity-70 hover:-translate-y-1 transition-transform active:scale-95"
+              src="/logo.png"
+              alt="Logo"
+              width={140}
+              height={65}
+            />
           </a>
           <p>
             {' '}
