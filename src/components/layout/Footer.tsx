4d51848356ad4c68943d2a05ec3870ce518a5914
DaCodeWiz <hello@jpearson.art>
2023-08-28 15:14:03 -0700

    changes

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -7,16 +7,17 @@ import { AiFillInstagram } from 'react-icons/ai';
 // Footer Component: Represents the footer section of the web page.
 const Footer = (): JSX.Element => {
   return (
-    <footer className="relative flex h-full flex-col bg-customWhite px-8 pb-8 pt-8 text-base sm:px-16 sm:text-lg xl:px-40 xl:pt-16">
+    <footer className="relative flex h-full flex-col bg-customWhite px-6 pb-8 pt-6 text-base sm:px-16 sm:text-lg xl:px-40 xl:pt-16">
       <div className="mb-4 flex flex-wrap justify-between xl:mb-24 xl:mt-4">
         <section className="w-full space-y-8 pb-8 sm:w-1/2 xl:w-5/12">
           <a className="block hover:cursor-pointer" href="/">
             <img
-              className="duration-300 ease-in-out hover:opacity-70 hover:-translate-y-1 transition-transform active:scale-95"
+              className="relative hover:opacity-90 transition duration-300 ease-in-out hover:scale-110 origin-center"
               src="/logo.png"
               alt="Logo"
               width={140}
               height={65}
+              loading='lazy'
             />
           </a>
           <p>
@@ -36,7 +37,7 @@ const Footer = (): JSX.Element => {
                 </p>
               </div>
             </div>
-            <br></br>
+            <br />
             <div className="flex">
               <FaPhoneAlt className="contactIcon" />
               <div className="xl:flex xl:space-x-1">
@@ -101,9 +102,7 @@ const Footer = (): JSX.Element => {
             <FaFacebookF className="socialMediaIcon" />
           </a>
         </div>
-        <p className="py-4">
-          Copyright by A Better Weigh Inc. All rights reserved.
-        </p>
+        <p className="py-4">A Better Weigh Inc&copy;. All rights reserved.</p>
       </section>
     </footer>
   );
