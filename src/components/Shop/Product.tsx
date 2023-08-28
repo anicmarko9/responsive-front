0dea906203681b3da9ac682b4d74d2a9e813c20a
trba1810 <trba8l@live.com>
2023-08-28 16:19:23 +0200

    small fixes

--- src/components/Shop/Product.tsx
+++ src/components/Shop/Product.tsx
@@ -11,25 +11,26 @@ const Product = ({
   // A fallback image source in case the provided image is not found.
   const notFound = '../ImgNotFound.svg';
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-green-100">
+    <section className="max-w-sm rounded overflow-hidden shadow-lg bg-green-100">
       <figure>
         <img
           className="mx-auto w-9/12 md:h-[300px] rounded-lg"
           src={imgSrc || notFound}
           alt={title}
         />
+        <div className="px-6 py-4">
+          <figcaption className="font-bold text-xl mb-2">{title}</figcaption>
+          <p className="text-gray-700 text-base">{description}</p>
+        </div>
       </figure>
-      <div className="px-6 py-4">
-        <figcaption className="font-bold text-xl mb-2">{title}</figcaption>
-        <p className="text-gray-700 text-base">{description}</p>
-      </div>
+
       <div className="px-6 pt-4 pb-2">
         <h1 className="text-gray-700 text-4xl font-black">{price}</h1>
         <button className="w-full mt-6 mb-3 py-2 text-white font-semibold bg-gray-700 hover:shadow-xl duration-200 hover:bg-gray-800">
           Buy Now
         </button>
       </div>
-    </div>
+    </section>
   );
 };
 
