ab1eedf828ea7e7ff623aeab5d82efa31bd82e7c
Marko Anić <73799942+anicmarko9@users.noreply.github.com>
2023-08-28 20:39:18 +0200

    refactor: html tags

--- src/components/Shop/Product.tsx
+++ src/components/Shop/Product.tsx
@@ -18,12 +18,9 @@ const Product = ({
           src={imgSrc || notFound}
           alt={title}
         />
-        <div className="px-6 py-4">
-          <figcaption className="font-bold text-xl mb-2">{title}</figcaption>
-          <p className="text-gray-700 text-base">{description}</p>
-        </div>
+        <figcaption className="font-bold text-xl mb-2">{title}</figcaption>
       </figure>
-
+        <p className="text-gray-700 text-base">{description}</p>
       <div className="px-6 pt-4 pb-2">
         <h1 className="text-gray-700 text-4xl font-black">{price}</h1>
         <button className="w-full mt-6 mb-3 py-2 text-white font-semibold bg-gray-700 hover:shadow-xl duration-200 hover:bg-gray-800">
