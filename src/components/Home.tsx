0e3bd3418af660550de3ac997ac89c6b539a14c9
DaCodeWiz <hello@jpearson.art>
2023-08-28 16:30:19 -0700

    smoother scroll

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import Lenis from '@studio-freight/lenis';
 import LandingPage from './content/LandingPage';
 import Layout from './layout/Layout';
 
@@ -12,6 +12,21 @@ const LazyContentFAQ = React.lazy(() => import('./content/ContentFAQ'));
 // Home Component: Represents the home page of the website
 // Updating to a functional component
 function Home() {
+   useEffect(() => {
+     const lenis = new Lenis({
+       lerp: 0.075,
+       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+     });
+
+      lenis.on('scroll', (e: any) => {
+        console.log(e)
+      })
+      function raf(time: any) {
+       lenis.raf(time);
+       requestAnimationFrame(raf);
+      }
+     requestAnimationFrame(raf);
+   }, []);
   return (
     // Using the Layout component to structure the page
     <Layout>
