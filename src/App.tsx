a9f01dc1e25b0e12af5dd0b00815941e11c61fea
Marko Anic <anicmarko9>
2023-09-03 21:24:32 +0200

    feat: import new font & add path alias to its' directory

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App(): JSX.Element {
   }, [initBodyOverlayScrollbars]);
 
   return (
-    <div className="font-libre-baskerville relative">
+    <div className="font-heebo relative">
       <BrowserRouter>
         <Routes>
           <Route path="/not-found" element={<NotFound />} />
