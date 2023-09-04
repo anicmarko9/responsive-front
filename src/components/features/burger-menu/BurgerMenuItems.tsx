94649107b68b130cd7998c5967a4a3383198d99b
SAFWAT <safwathkhan2@gmail.com>
2023-09-05 01:41:22 +0530

    feat: Enhance Hamburger Menu Animation on Close #160

--- src/components/features/burger-menu/BurgerMenuItems.tsx
+++ src/components/features/burger-menu/BurgerMenuItems.tsx
@@ -4,9 +4,9 @@ import { IBurgerMenuItem } from '@Types/interfaces';
 
 const BurgerMenuItem = ({ href, icon, text }: IBurgerMenuItem): JSX.Element => {
   return (
-    <a className="hoverUnderline flex items-center py-2" href={href}>
-      <span className="text-secondary">{icon}</span>
-      <span className="ml-1">{text}</span>
+    <a className="hoverUnderline flex items-center py-2 text-secondary" href={href}>
+      {icon}
+      <span className='ml-1 text-black'>{text}</span>
     </a>
   );
 };
