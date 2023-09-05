c2ebc8e629a9265a9e3c3aabadcfb048ccc7257a
Marko Anić <73799942+anicmarko9@users.noreply.github.com>
2023-09-05 17:57:24 +0200

    refactor: change "p" to "figcaption" HTML tag

--- src/components/content/programs/Program.tsx
+++ src/components/content/programs/Program.tsx
@@ -18,9 +18,9 @@ const Program: React.FC<IProgram> = ({
           src={imgSrc || notFound}
           alt={programName}
         />
-        <p className="absolute bottom-0 left-0 w-full bg-black/60 flex flex-col justify-center items-center font-libre-baskerville py-1 px-3 text-sm text-white hover:text-yellow-300 transition-all ease-in-out">
+        <figcaption className="absolute bottom-0 left-0 w-full bg-black/60 flex flex-col justify-center items-center font-libre-baskerville py-1 px-3 text-sm text-white hover:text-yellow-300 transition-all ease-in-out">
           {description}
-        </p>
+        </figcaption>
         <div className="absolute top-0 left-0 w-full bg-black/60 flex flex-row justify-between p-1 items-center font-libre-baskerville text-white uppercase hover:text-yellow-300 transition-all ease-in-out">
           <h2 className="font-bold ml-1">{programName}</h2>
           <p className="text-sm text-center mr-1">{duration}</p>
