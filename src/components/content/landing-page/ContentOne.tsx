a088e8310b305c391475a824c7c94ed8c753bb5f
Marko Anic <anicmarko9>
2023-09-16 16:57:44 +0200

    refactor: padding of landing page & navbar

--- src/components/content/landing-page/ContentOne.tsx
+++ src/components/content/landing-page/ContentOne.tsx
@@ -6,7 +6,7 @@ import MainButton from '@Components/features/MainButton';
 const ContentOne = (): JSX.Element => {
   return (
     // Section container for the first content section with responsive styling.
-    <section className="relative flex h-full flex-col justify-between bg-secondary px-8 pb-4 text-lg text-customWhite sm:px-36 xl:flex-row xl:px-36 xl:pb-0">
+    <section className="relative flex h-full flex-col justify-between bg-secondary px-8 pb-4 text-lg text-customWhite sm:px-16 xl:flex-row xl:px-36 xl:pb-0">
       <div className="my-8 flex flex-col space-y-8 sm:my-12 sm:space-y-12 xl:my-16 xl:w-1/2 xl:space-y-16">
         <div className="space-y-4 sm:space-y-6">
           <h2 className="contentOneTitle">Who Is The Consultation For?</h2>
--- src/components/content/landing-page/ContentTwo.tsx
+++ src/components/content/landing-page/ContentTwo.tsx
@@ -7,10 +7,10 @@ const ContentTwo = (): JSX.Element => {
   return (
     // Section container for the second content section with responsive styling.
     <section className="relative flex h-full flex-col items-center space-y-8 bg-white px-0 py-8 text-start text-lg xl:px-36 xl:pb-0 xl:pt-16 xl:text-center">
-      <h2 className="order-first my-2 w-full px-8 text-3xl tracking-wide sm:px-36 xl:order-none xl:my-4 xl:px-0 xl:text-4xl">
+      <h2 className="order-first my-2 w-full px-8 text-3xl tracking-wide sm:px-16 xl:order-none xl:my-4 xl:px-0 xl:text-4xl">
         We Can Help You
       </h2>
-      <p className="flex flex-col space-y-4 px-8 sm:space-y-8 sm:px-36 xl:block xl:space-y-0 xl:px-0">
+      <p className="flex flex-col space-y-4 px-8 sm:space-y-8 sm:px-16 xl:block xl:space-y-0 xl:px-0">
         <span className="order-1 text-sm sm:text-base xl:order-none xl:text-lg">
           {/* First part of the message about weight loss challenges and common experiences. */}
           For those of us who struggle with our weight it&apos;s easy to lose
@@ -26,7 +26,7 @@ const ContentTwo = (): JSX.Element => {
         </span>
       </p>
       {/* Additional Information */}
-      <p className="order-4 px-8 text-sm sm:px-36 sm:text-base xl:order-none xl:px-0 xl:text-lg">
+      <p className="order-4 px-8 text-sm sm:px-16 sm:text-base xl:order-none xl:px-0 xl:text-lg">
         You can join the ranks of our over 15,000 satisfied clients who have
         lost weight, regained their confidence and are living happy exuberant
         lives with their family. They did this without: breaking the bank,
