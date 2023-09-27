ded81f3f17910a31b12468cf2e4506d73ce5f699
Marko Anic <anicmarko9>
2023-09-27 19:50:49 +0200

    fix: bookings spacing

--- src/pages/Booking.tsx
+++ src/pages/Booking.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Lenis from '@studio-freight/lenis';
 
 import Layout from '@Layout/Layout';
 import BookOnline from '@Components/content/booking/BookOnline';
 
 const Booking = (): JSX.Element => {
+  useEffect(() => {
+    const lenis: Lenis = new Lenis({
+      lerp: 0.075,
+      easing: (t: number): number => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    });
+
+    const raf = (time: number): void => {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    };
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
     <Layout>
-      <section className="mx-auto mt-3 flex w-screen flex-col flex-wrap gap-5 bg-primary px-4 py-10 md:px-16 lg:px-32 xl:px-56">
-        <BookOnline />
-      </section>
+      <BookOnline />
     </Layout>
   );
 };
