276f5c9202dba1c2bd727c36cf1c0b818640945e
Marko Anic <anicmarko9>
2023-09-29 17:32:19 +0200

    refactor: blog spacing

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -7,7 +7,7 @@ import { posts } from '@Mocks/blog-posts';
 const Blog = (): JSX.Element => {
   return (
     <Layout>
-      <section className="mx-auto mt-3 flex w-screen flex-col flex-wrap gap-5 px-4 py-10 md:px-16 lg:px-32 xl:px-56">
+      <section className="baseContainerX baseContainerY">
         <BlogContainer posts={posts} />
       </section>
     </Layout>
